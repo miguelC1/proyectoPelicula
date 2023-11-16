95425207f9a5d7cb50c0cbb753c7fb56f8c62497
fernando03 <jc.fernando03@gmail.com>
2023-11-16 07:52:47 -0400

    creacion de cards y llenado  estaticamente

--- src/app/pages/categorias/categorias.page.ts
+++ src/app/pages/categorias/categorias.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DrawerScreen } from 'src/app/Modelos/DrawerScreen';
 
 @Component({
   selector: 'app-categorias',
@@ -6,7 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categorias.page.scss'],
 })
 export class CategoriasPage implements OnInit {
-
+  
+  appPages: DrawerScreen[] = [
+    { name: 'Home', icon: 'home', url: '/menu/home' },
+    {
+      name: 'Help',
+      icon: 'people-circle-sharp',
+      isAsset: true,
+      url: '/menu/help',
+    },
+    { name: 'Feedback', icon: 'help', url: '/menu/feedback' },
+    { name: 'Invite Friend', icon: 'group', url: '/menu/invite-friend' },
+    { name: 'Rate the app', icon: 'share', url: undefined },
+    { name: 'About Us', icon: 'info', url: undefined },
+  ];
   constructor() { }
 
   ngOnInit() {
